e52709883d76d141c2708121c78c061dbc5f497e
Artem Artemyev <viiv.c58@gmail.com>
2016-02-04 01:34:37 +0500

    minor example fixes

--- examples/js/models/Todo.model.js
+++ examples/js/models/Todo.model.js
@@ -3,9 +3,10 @@ import Collection from './Collection';
 const TodoModel = new Collection('todos');
 export default TodoModel;
 
-console.log(TodoModel.cursorClass);
-TodoModel.insert({text: 'Todo #1', complete: false});
-TodoModel.insert({text: 'Todo #2', complete: false});
-TodoModel.insert({text: 'Todo #3', complete: false});
-TodoModel.insert({text: 'Todo #4', complete: false});
-TodoModel.insert({text: 'Todo #5', complete: false});
+if (typeof window === 'undefined') {
+  TodoModel.insert({text: 'Todo #1', complete: false});
+  TodoModel.insert({text: 'Todo #2', complete: false});
+  TodoModel.insert({text: 'Todo #3', complete: false});
+  TodoModel.insert({text: 'Todo #4', complete: false});
+  TodoModel.insert({text: 'Todo #5', complete: false});
+}
